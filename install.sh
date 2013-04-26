0a85096903e3a96c5fe90ecc7715392a40d9d48b
Christof Glaser <gcg@gl.aser.de>
2013-04-26 23:40:39 +0200

    Add Sublime Edit config for Flatland theme and a nifty color scheme.
    Find the Flatland theme here: https://github.com/thinkpixellab/flatland
    
    The config file just tweaks some Flatland parameters. Most notably: tabs show more text and save some pixels in height.
    
    The colour scheme is a heavily edited version of All Hallow's Eve (from TextMate), matching the Flatland colors.
    It works best with a font that provides an nice italic version, like Microsoft Consolas.

--- install.sh
+++ install.sh
@@ -12,6 +12,13 @@ do
 	[ -f $df ] && mv $df $df-$(date +%y%m%d-%H%M) && echo "Made backup of $df."
 	ln -svf $f $df
 done
+
+sublime_library=~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+if [ -d "$sublime_library" ]; then
+  ln -vs $INSTALL_DIR/Sublime\ Edit\ 2/* "$sublime_library"
+  echo "Please edit your Sublime Edit config to use the Flatland theme and My Hallow's Flat color scheme."
+fi
+
 if [ -n "$EDITOR" ]; then
 	$EDITOR $HOME/.gitconfig
 else
