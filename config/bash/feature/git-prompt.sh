1caef6b16b162b8faafa1d69edc58a56b9e7f91a
Christof Glaser <gcg@gl.aser.de>
2017-08-07 00:54:24 +0200

    Simplify git prompt, make those symbols work again.

--- config/bash/feature/git-prompt.sh
+++ config/bash/feature/git-prompt.sh
@@ -1,29 +1,24 @@
 # Certain Barking at Trees
-function prompt_git_branch () {
-  local branch=$(git branch 2>/dev/null)
-  if [ -n "$branch" ]; then
-    branch=$(echo "$branch" | grep '^*')
-    branch="(${branch:2})"
-  else
-    branch=''
-  fi
-  echo $branch
-}
-
-# AUTHOR: Geoffrey Grosenbach http://peepcode.com, from http://pastie.org/325104
-# Extended to give zero or more different symbols as per git status.
-
 # Get the name of the branch we are on
 function git_prompt_info () {
   local branch_prompt=$(prompt_git_branch)
   if [ -n "$branch_prompt" ]; then
     local status_icon=$(git_status)
-    echo "$branch_prompt$status_icon"
+    echo "($branch_prompt)$status_icon"
   fi
 }
+
+function prompt_git_branch () {
+  local GIT_BRANCH=$(git branch 2>/dev/null | grep '^*')
+  GIT_BRANCH=${GIT_BRANCH:2}
+  echo $GIT_BRANCH
+}
+
 # Show character if changes are pending
+# AUTHOR: Geoffrey Grosenbach http://peepcode.com, from http://pastie.org/325104
+# Extended to give zero or more different symbols as per git status.
 function git_status () {
-  local current_git_status=$(git status | egrep '^# [A-Z]' 2> /dev/null)
+  local current_git_status=$(git status 2>/dev/null)
   local tag=""
   case "$current_git_status" in
     *Changes\ to\ be\ committed*) tag="$tag$lightyellow ✔" ;;
